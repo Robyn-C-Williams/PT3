5b15041967abc90265b70fbbc8f1d4055f83cdb0
Robyn Williams <155536668+Robyn-C-Williams@users.noreply.github.com>
2024-03-24 16:37:59 +0000

    Update lottoTicketT23.py

--- lottoTicketT23.py
+++ lottoTicketT23.py
@@ -1,7 +1,8 @@
 # This is a lottery ticket generator 
 import random
 
-
+# I have created the userInput branch
+username = input("Please enter your name: ")
 
 # The amount of numbers the ticket must have
 ticket_numbers = 6
