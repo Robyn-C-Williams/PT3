f6605ce91074782eeaa9f9bfad9d132266aa483c
Robyn Williams <155536668+Robyn-C-Williams@users.noreply.github.com>
2024-03-24 16:11:12 +0000

    Add files via upload

---
+++ lottoTicketT23.py
@@ -0,0 +1,13 @@
+# This is a lottery ticket generator 
+import random
+
+
+
+# The amount of numbers the ticket must have
+ticket_numbers = 6
+
+# print random numbers
+for i in range(ticket_numbers):
+    print (random.randint(1,59))
+
+
